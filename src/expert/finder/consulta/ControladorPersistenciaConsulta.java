e314cc70ad57c57306d91a14552c7cecca72281c
RubenBagan <ruben.bagan@gmail.com>
2016-05-30 18:06:34 +0200

    Mejora de documentacion y algunos bugfix

--- src/expert/finder/consulta/ControladorPersistenciaConsulta.java
+++ src/expert/finder/consulta/ControladorPersistenciaConsulta.java
@@ -4,30 +4,64 @@
  * and open the template in the editor.
  */
 package expert.finder.consulta;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
  *
  * @author Ruben Bagan Benavides
  */
+
+/**
+ * La funcio d'aquesta clase es la de permetre importar i exportar consultes com a objecte.
+ */
 public class ControladorPersistenciaConsulta {
-    public void exportar_consulta(String nomFitxer, Consulta consulta) throws IOException {
-        FileOutputStream fout = new FileOutputStream(nomFitxer);
+    /**
+     * Exporta el conjunt de consultes pasades com a parametre a un objecte anomenat Consultes.av que es crea en la
+     * propia arrel del programa.
+     * @param consultes Conte un conjunt de consultes. El valor no pot ser nul.
+     * @throws IOException
+     */
+    public void exportar_consulta(ArrayList<Consulta> consultes) throws IOException {
+        FileOutputStream fout = new FileOutputStream("Consultes.sav");
         ObjectOutputStream oos;
         try {
             oos = new ObjectOutputStream(fout);
-            oos.writeObject(consulta);
+            oos.writeObject(consultes);
+            fout.close();
+            oos.close();
         } catch (IOException e) {
-            throw new IOException("Error: No s'ha pogut exportar la consulta en la ruta (" + nomFitxer +")"+
-                    "perque o no existeix, o es utilitzat per un altre programa.");
+            System.out.println(e.getMessage());
         }
-        fout.close();
-        oos.close();
     }
+
+    /**
+     * Importa un conjunt de consultes
+     * @return
+     * @throws IOException
+     */
+    public ArrayList<Consulta> importar_consulta() throws IOException {
+        FileInputStream fitxer = new FileInputStream("Consultes.sav");
+        ObjectInputStream ois;
+        ArrayList<Consulta> consultes;
+        try {
+            ois = new ObjectInputStream(fitxer);
+            consultes = (ArrayList<Consulta>) ois.readObject();
+        } catch (IOException e) {
+            throw new IOException("Error: No s'ha pogut importar el conjunt de consultes enmmagatzemat en la " +
+                    "propia arrel del programa amb nom Consultes.sav. Perque o no  existeix, o es utilitzat per un " +
+                    "altre programa.");
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Error: El fitxer binari on estan enmmagatzemades les consultes en l'arrel del " +
+                    "programa amb nom Consultes.sav no s'han pogut obtenir perquè el contingut del fitxer no es un " +
+                    "conjunt de consultes.");
+        }
+
+        ois.close();
+        fitxer.close();
+        return consultes;
+    }
+
+
     
 }
--- src/expert/finder/utils/ControladorPersistenciaMatriu.java
+++ src/expert/finder/utils/ControladorPersistenciaMatriu.java
@@ -1,7 +1,4 @@
 package expert.finder.utils;
-
-import expert.finder.graf.Graf;
-
 import java.io.*;
 
 /**
@@ -9,14 +6,15 @@ import java.io.*;
  */
 
 /**
- *
+ * La funcio d'aquesta clase es la de poder importar y exportar matrius com a objecte.
  */
 public class ControladorPersistenciaMatriu {
     /**
-     * Retorna una referencia a una martiu de clausura enmmagatzemada en la ruta pasada com a paramtre.
+     * Retorna una referencia a una martiu de clausura enmmagatzemada en la propia arrel del projecte amb el nom
+     * igual al nom de la ruta pasada per parametre. El cost d'executar aquesta funcio es: O(1).
      * @param rutaFixer Conte el nom de la matriu de clausura.
      * @return Retorna una referencia a la matriu de clausura.
-     * @throws IOException El fitxer on s'enmmagatzema la matriu de clausura no conte una matriu i/o la ruta noe s
+     * @throws IOException El fitxer on s'enmmagatzema la matriu de clausura no conte una matriu i/o la ruta no es
      * valida.
      */
     public Matriu importar(String rutaFixer) throws IOException {
@@ -41,10 +39,11 @@ public class ControladorPersistenciaMatriu {
     }
 
     /**
-     *
-     * @param rutaFitxer
-     * @param m
-     * @throws IOException
+     * Enmmagatzema una instancia de la referencia a una matriu pasada per parametre amb nom igual que el del paramtre.
+     * El cost d'executar aquesta funcio es: O(1).
+     * @param rutaFitxer Conte el nom de la matriu de clausura.
+     * @param m Conte una referencia a la matriu que s'ha d'enmmagatzemar
+     * @throws IOException No s'ha pogut exportar perque la ruta no existeix.
      */
     public void exportar(String rutaFitxer, Matriu m) throws IOException {
         FileOutputStream fout = new FileOutputStream(rutaFitxer);
